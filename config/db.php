734ca8388c922f6741d012f5d31c1d06ae2864d9
Nguyen Pham Viet Duc <phamvietduc231st@gmail.com>
2026-06-08 10:39:17 +0700

    feat: setup environment configuration, load dotenv and configure database

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => \yii\db\Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => 'mysql:host=' . ($_ENV['DB_HOST'] ?? 'localhost') . ';port=' . ($_ENV['DB_PORT'] ?? '3306') . ';dbname=' . ($_ENV['DB_NAME'] ?? 'yii2basic'),
+    'username' => $_ENV['DB_USER'] ?? 'root',
+    'password' => $_ENV['DB_PASSWORD'] ?? '',
+    'charset' => $_ENV['DB_CHARSET'] ?? 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
--- web/index.php
+++ web/index.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+require __DIR__ . '/../vendor/autoload.php';
+if (class_exists('Dotenv\Dotenv')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->safeLoad();
+}
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
