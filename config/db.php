346a5b6e92282902348b97eb1f2a90ad9c29932b
Nguyen Za Thu <nguyengiathu24052004@gmail.com>
2026-06-08 10:33:26 +0700

    chore(config): setup environment with vlucas/phpdotenv
    Install phpdotenv and setup .env files
    Ignore .env in git
    Load env vars in entry scripts (index.php, yii)
    Update db config to use $_ENV

--- config/db.php
+++ config/db.php
@@ -1,14 +1,16 @@
 <?php
 
-return [
-    'class' => \yii\db\Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+declare(strict_types=1);
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+return [
+    'class'    => \yii\db\Connection::class,
+    'dsn'      => "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}",
+    'username' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASS']    ?? '',
+    'charset'  => $_ENV['DB_CHARSET'] ?? 'utf8mb4',
+
+    // Schema cache options (enable for production)
+    //'enableSchemaCache'   => true,
+    //'schemaCacheDuration' => 3600,
+    //'schemaCache'         => 'cache',
 ];
