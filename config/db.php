b259c37648421cab0abb8c2dbf8a25f375d37964
Nguyen Pham Viet Duc <phamvietduc231st@gmail.com>
2026-06-12 15:10:49 +0700

    fix(config): load database username and environment dynamically from env

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => \yii\db\Connection::class,
     'dsn' => 'mysql:host=' . ($_ENV['DB_HOST'] ?? 'localhost') . ';port=' . ($_ENV['DB_PORT'] ?? '3306') . ';dbname=' . ($_ENV['DB_NAME'] ?? 'yii2basic'),
-    'username' => $_ENV['DB_USER'] ?? 'root',
+    'username' => $_ENV['DB_USERNAME'] ?? 'root',
     'password' => $_ENV['DB_PASSWORD'] ?? '',
     'charset' => $_ENV['DB_CHARSET'] ?? 'utf8mb4',
 
--- web/index.php
+++ web/index.php
@@ -8,8 +8,8 @@ if (class_exists('Dotenv\Dotenv')) {
     $dotenv->safeLoad();
 }
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', isset($_ENV['YII_DEBUG']) ? filter_var($_ENV['YII_DEBUG'], FILTER_VALIDATE_BOOLEAN) : true);
+defined('YII_ENV') or define('YII_ENV', isset($_ENV['YII_ENV']) ? strtolower($_ENV['YII_ENV']) : 'dev');
 
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
